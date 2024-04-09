3ca084f62fb4a80f1dfd4a4ce96a34cb137f3e1e
alex-manev <alkesmanev2005@gmail.com>
2024-04-09 21:13:59 +0300

    Component folder added

---
+++ SkillReceive/Component/MainMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkillReceive.Component
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+
+        }
+    }
+}
