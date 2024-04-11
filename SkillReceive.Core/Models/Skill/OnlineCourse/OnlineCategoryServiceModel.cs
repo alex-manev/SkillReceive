f50bb26c5a862197fa63e16d1c9c719942ff8537
alex-manev <alkesmanev2005@gmail.com>
2024-04-12 02:55:38 +0300

    Online Course View models created

---
+++ SkillReceive.Core/Models/Skill/OnlineCourse/OnlineCategoryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillReceive.Core.Models.Skill.OnlineCourse
+{
+    public class OnlineCategoryServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- SkillReceive.Core/Models/Skill/OnlineCourse/OnlineFormModel.cs
+++ SkillReceive.Core/Models/Skill/OnlineCourse/OnlineFormModel.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static SkillReceive.Core.Constants.MessageConstants;
+using static SkillReceive.Infrastructure.Constants.DataConstants.OnlineCourses.OnlineCourse;
 
 namespace SkillReceive.Core.Models.Skill.OnlineCourse
 {
     public class OnlineFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(TitleMaxLength,
+            MinimumLength = TitleMinLength,
+            ErrorMessage = LengthMessage)]
 
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(DescriptionMaxLength,
+            MinimumLength = DescriptionMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(NeededTechnologiesMaxLength,
+            MinimumLength = NeededTechnologiesMinLength,
+            ErrorMessage = LengthMessage)]
+        public string NeededTechnologies { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Range(typeof(decimal),"0.00",
+            PricePerMonthMaximum,
+            ErrorMessage = "Price per month must be a positive number and less than {2}lv.")]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Image URL")]
+        public string ImageURL { get; set; } = null!;
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<OnlineCategoryServiceModel> Categories { get; set; } = new List<OnlineCategoryServiceModel>();
     }
 }
