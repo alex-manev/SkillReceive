3173b4b2e0aec74ad2b676f2874d7be3e49cd1b4
alex-manev <alkesmanev2005@gmail.com>
2024-04-12 03:22:46 +0300

    MustBeCreator Attribute added

---
+++ SkillReceive/Attributes/MustBeCreator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using SkillReceive.Core.Contracts.Creator;
+using System.Security.Claims;
+using SkillReceive.Controllers;
+
+namespace SkillReceive.Attributes
+{
+    public class MustBeCreator : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            ICreatorService? creatorService = context.HttpContext.RequestServices.GetService<ICreatorService>();
+
+            if (creatorService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (creatorService != null && creatorService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(CreatorController.Become), "Creator", null);
+            }
+
+
+        }
+    }
+}
