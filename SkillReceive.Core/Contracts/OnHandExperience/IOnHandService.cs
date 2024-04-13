529f78b6f954bf90d6eb8adbeccbed8b4d3b7a0c
alex-manev <alkesmanev2005@gmail.com>
2024-04-13 16:40:16 +0300

    Adding On Hand Service

---
+++ SkillReceive.Core/Contracts/OnHandExperience/IOnHandService.cs
@@ -0,0 +1,13 @@
+﻿using SkillReceive.Core.Models.Skill.OnlineCourse;
+
+namespace SkillReceive.Core.Contracts.OnHandExperience
+{
+    public interface IOnHandService
+    {
+        Task<IEnumerable<OnlineCategoryServiceModel>> AllCategoriesAsync();
+
+        Task<bool> CategoryExistsAsync(int categoryId);
+
+        Task<int> CreateAsync(OnlineFormModel model, int creatorId);
+    }
+}
---
+++ SkillReceive.Core/Services/OnHandExperience/OnHandService.cs
@@ -0,0 +1,23 @@
+﻿using SkillReceive.Core.Contracts.OnHandExperience;
+using SkillReceive.Core.Models.Skill.OnlineCourse;
+
+namespace SkillReceive.Core.Services.OnHandExperience
+{
+    public class OnHandService : IOnHandService
+    {
+        public Task<IEnumerable<OnlineCategoryServiceModel>> AllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateAsync(OnlineFormModel model, int creatorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
