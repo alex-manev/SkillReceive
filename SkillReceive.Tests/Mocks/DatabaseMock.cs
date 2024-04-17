def4be72b9549c64369faedfaf1e3c1815077ce1
alex-manev <alkesmanev2005@gmail.com>
2024-04-17 23:59:26 +0300

    Database mock added

---
+++ SkillReceive.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillReceive.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillReceive.Tests.Mocks
+{
+    public class DatabaseMock
+    {
+        public static SkillReceiveDbContext Instance 
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<SkillReceiveDbContext>()
+                    .UseInMemoryDatabase("SkillReceiveInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new SkillReceiveDbContext(dbContextOptions, false);
+            }
+        }
+    }
+}
