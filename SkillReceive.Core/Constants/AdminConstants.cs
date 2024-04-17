85952220de7f02418d0950fcfbc5cb4b7aec10b0
alex-manev <alkesmanev2005@gmail.com>
2024-04-17 16:07:16 +0300

    Core constants modified

---
+++ SkillReceive.Core/Constants/AdminConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillReceive.Core.Constants
+{
+    public static class AdminConstants
+    {
+        public const string AdminRole = "Administrator";
+
+        public const string AdminAreaName = "Admin";
+
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
--- SkillReceive.Core/Constants/RoleConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SkillReceive.Core.Constants
-{
-    public static class RoleConstants
-    {
-        public const string AdminRole = "Administrator";
-    }
-}
